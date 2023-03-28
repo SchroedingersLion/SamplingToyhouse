3295d4453c4fc1c868dbbd2d597dbafbbc1b9e45
Rene Lohmann <R.Lohmann@ed.ac.uk>
2023-03-28 12:39:30 +0200

    added cmath header to measuremnts.h

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main(int argc, char *argv[]){
     double gamma = 20;
     double h = 0.01;
 
-    int iter = 100000;       // Number of iteration (sampler steps).
+    int iter = 20000;       // Number of iterations (sampler steps).
     int t_meas = 1;          // Take measurement and use it for on-the-fly time-average every t_meas iterations.
-    int n_dist = 1000;       // Store and print-out any n_dist taken measurement. 
-    bool t_avg = true;
+    int n_dist = 1;       // Store and print-out any n_dist taken measurement. 
+    bool t_avg = false;
 
     // ### CONSTRUCT ONE OF THE SAMPLERS DEFINED IN "samplers.h". ### //
     OBABO_sampler testsampler(T, gamma, h);    
@@ -20,17 +20,16 @@ int main(int argc, char *argv[]){
     
     // ### CONSTRUCT THE PROBLEM TO BE SAMPLED ON, DEFINED IN "problems.h". ### //
     HARMONIC_OSCILLATOR_1D testproblem;
-
+    
     
     // ### CONSTRUCT MEASUREMENT OBJECT DEFINED IN "measurements.h". ### //
-    MEASUREMENT_HO_1D RESULTS(n_dist, t_avg);
+    MEASUREMENT_DEFAULT RESULTS(n_dist, t_avg);
 
     std:: string outputfile = "RESULTS.csv";  // Name of output file.
 
 
     // ### RUN SAMPLER. ### //
     testsampler.run_mpi_simulation(argc, argv, iter, testproblem, RESULTS, outputfile, t_meas);          
-
 
 
     return 0;
--- measurements.h
+++ measurements.h
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <cmath>
 
 
 class IMEASUREMENT{
